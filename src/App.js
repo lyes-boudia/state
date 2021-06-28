b87045fd183763ed06d25051faccfedef4be4b36
lyes <lyes.boudia@gmail.com>
2021-06-28 12:28:53 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import react from "react"
+import { useState } from "react";
+import user from "./user.png"
 import './App.css';
 
-function App() {
+class App extends  react.Component{
+  state = {
+    person: {
+      fullName: "JHON",
+      imgProfile: user,
+      profession: "software developper",
+      bio: "I am developper..."
+    },
+    counter: 0,
+    show: false
+  }
+  incrementCounter = () => {
+    const {counter} = this.state
+    this.setState({counter: counter + 1})
+  }
+  componentDidMount () {
+    setInterval(this.incrementCounter, 1000)
+  }
+  handleClick = () => this.setState({show: !this.state.show})
+render() {
+  const styleprofile = {
+    color: '#FFFFFF',
+    display: 'flex',
+    margin: '40px 60px',
+    textAlin: 'start',
+    justifyitem: 'center'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Component has mounted</h1>
+      <h2>{this.state.counter}</h2>
+      <button onClick={this.handleClick}>{this.state.show ? 'Hide profile': 'Show profile'}</button>
+      {(this.state.show) &&
+   <div style={styleprofile}>
+    <div>
+      <img src={user} alt="Profile"></img>
+    </div>
+    <div style={{margin: '20px 50px'}}>
+<h1>{this.state.person.fullName}</h1>
+<h3>{this.state.person.profession}</h3>
+<span>{this.state.person.bio}</span>
+
+    </div>
+    </div>
+}
     </div>
   );
+}
 }
 
 export default App;
